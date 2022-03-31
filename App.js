192681db98dc7e750544a676243e8cce951bf153
Deotama <deotama11@gmail.com>
2022-03-31 16:27:04 +0700

    8 - Navigator

--- App.js
+++ App.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
-import {RestaurantsScreen} from './src/features/restaurants/screens/restaurants.screen';
 import {theme} from './src/infrastructure/theme/index';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {RestaurantsContextProvider} from './src/services/restaurants/restaurants.context';
 import {LocationContextProvider} from './src/services/location/location.context';
-
-function SettingsScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-function MapScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Map!</Text>
-    </View>
-  );
-}
-const Tab = createBottomTabNavigator();
+import {Navigation} from './src/infrastructure/navigation';
 
 const App = () => {
   return (
@@ -33,30 +12,7 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <RestaurantsContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator
-                screenOptions={({route}) => ({
-                  tabBarIcon: ({focused, color, size}) => {
-                    let iconName;
-                    if (route.name === 'Restaurants') {
-                      iconName = focused ? 'ios-restaurant' : 'ios-restaurant';
-                    } else if (route.name === 'Map') {
-                      iconName = focused ? 'ios-map' : 'ios-map';
-                    } else {
-                      iconName = focused ? 'ios-options' : 'ios-options';
-                    }
-                    return (
-                      <Ionicons name={iconName} size={size} color={color} />
-                    );
-                  },
-                  tabBarActiveTintColor: 'tomato',
-                  tabBarInactiveTintColor: 'gray',
-                })}>
-                <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-                <Tab.Screen name="Map" component={MapScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {Text, View} from 'react-native';
+
+import {RestaurantsScreen} from '../../features/restaurants/screens/restaurants.screen';
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function MapScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Map!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Restaurants') {
+              iconName = focused ? 'ios-restaurant' : 'ios-restaurant';
+            } else if (route.name === 'Map') {
+              iconName = focused ? 'ios-map' : 'ios-map';
+            } else {
+              iconName = focused ? 'ios-options' : 'ios-options';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
